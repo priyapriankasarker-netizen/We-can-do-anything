6bdb9e95992ca824630f2b0c677db5da6d429cd2
priyapriankasarker-netizen <priyapriankasarker@gmail.com>
2026-03-20 03:44:31 +0600

    Create App.js as the main entry point for the admin mobile app

---
+++ App.js
@@ -0,0 +1,7 @@
+// Main entry point for the admin mobile app
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+AppRegistry.registerComponent('AdminMobileApp', () => App);
